98c690c1a544a900185b21671fc387d641de9170
canguruhh <sven@mutzl.org>
2019-03-07 00:07:42 +0100

    stop reuse api connection

--- src/libraries/prismic.ts
+++ src/libraries/prismic.ts
@@ -1,20 +1,17 @@
 import { RichText } from 'prismic-dom'
 import { getApi, Predicates } from 'prismic-javascript'
-import ResolvedApi, { QueryOptions } from 'prismic-javascript/d.ts/ResolvedApi'
+import { QueryOptions } from 'prismic-javascript/d.ts/ResolvedApi'
 import { API_ENDPOINT, API_TOKEN } from '../config/prismic'
 
 const VALID_LANGUAGES = ['en-us', 'zh-cn']
 
 export class Prismic {
-    public static api: ResolvedApi
     public static async documents(query: { conditions: Array<[string, string]>, options: QueryOptions }) {
         if (query.options.lang && VALID_LANGUAGES.indexOf(query.options.lang.toString()) === -1) {
             throw Error('invalid language')
         }
-        if (!this.api) {
-            this.api = await getApi(API_ENDPOINT, { accessToken: API_TOKEN })
-        }
-        const response = await this.api.query(
+        const api = await getApi(API_ENDPOINT, { accessToken: API_TOKEN })
+        const response = await api.query(
             query.conditions.map(([condition, value]) => Predicates.at(condition, value)),
             query.options,
         )
